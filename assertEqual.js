230a50b37c4b321ff4f6d8ade983017dc3a7b4f3
Duncan Haran <duncan.haran@gmail.com>
2019-06-24 20:15:26 +0000

    updated to use template strings instead of concatenation

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("💯️💯️💯️ Assertion passed: " + actual + " === " + expected);
+    console.log(`💯️💯️ Assertion passed: ${actual} === ${expected}`);
   } else {
-    console.log("🤬️🤬️🤬️ Assertion failed: " + actual + " !== " + expected);
+    console.log(`🤬️🤬️🤬️ Assertion failed: ${actual} !== ${expected}`);
   }
 };
 
