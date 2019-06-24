0ea640a5cff1ea8c8a202d98a4f478a2b9c9d3f0
Duncan Haran <duncan.haran@gmail.com>
2019-06-24 20:02:22 +0000

    emoji assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("💯️💯️💯️ Assertion passed: " + actual + " === " + expected);
+  } else {
+    console.log("🤬️🤬️🤬️ Assertion failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
