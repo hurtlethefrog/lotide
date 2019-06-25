fb666d29d1c5c06c33fc34a93c0c3e05126a2df9
Duncan Haran <duncan.haran@gmail.com>
2019-06-25 23:12:14 +0000

    cleaned up the code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,6 @@
-const assertArraysEqual = function(array1, array2, expected) {  
-  for (let i = 0; i < array1.length || i < array2.length; ++i) {
-      if (array1[i] !== array2[i]) {
+const assertArraysEqual = function(actual, expected,) {  
+  for (let i = 0; i < actual.length || i < expected.length; ++i) {
+      if (actual[i] !== expected[i]) {
         return console.log("nope");
       } 
     } return console.log("sure");
