eb300d7919837df7590dbbc3c2af6d09f27a943c
Duncan Haran <duncan.haran@gmail.com>
2019-06-27 16:34:12 +0000

    fixed assertEqualArray, and added object equivilent

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # lotide
 lighthouse library 
+
+## Contents
+
+* [assertArraysEqual](/home/lhl/lighthouse/w1/lotide/assertArraysEqual.js)
+
+* [assertEqual](assertEqual.js)
+
+* [countLetters](countLetters.js)
--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,17 @@
-const assertArraysEqual = function(actual, expected,) {  
-  for (let i = 0; i < actual.length || i < expected.length; ++i) {
-      if (actual[i] !== expected[i]) {
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length || i < array2.length; ++i) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    } 
+  } return true;
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual[0],actual[1]) !== expected) {
         return console.log("nope");
-      } 
-    } return console.log("sure");
+      } else { return console.log("sure");
   }
-assertArraysEqual([1,2,3],[1,2,"3"]);
+}
+
+assertArraysEqual([[1,2,3],[1,2,3]], true);
