a8484445583b49c195321e44ca90763131b55052
Duncan Haran <duncan.haran@gmail.com>
2019-07-01 20:01:47 +0000

    chai and mocha used for assertEq

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,3 +1,12 @@
 const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
 
-assertEqual(true, true);
+describe("#assertEqual", () => {
+  it("returns true for 'hello' and 'hello'", () => {
+    assert.strictEqual(assertEqual('hello', 'hello'));
+  });
+
+  it("returns false for 5 and '5'", () => {
+    assert.strictEqual(assertEqual('5', 5)); 
+  });
+});
