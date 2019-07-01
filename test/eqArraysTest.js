f2c8720c3e81bfc6ccbf8c27bfb71f8ca1b980f5
Duncan Haran <duncan.haran@gmail.com>
2019-07-01 19:54:05 +0000

    chai and mocha used for assertArrayEqTest

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,3 +1,12 @@
 const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 
-console.log(eqArrays([1,2,3], [1,2,3]))
+describe("#eqArrays", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1,2,3]), true);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.deepEqual(eqArrays(['5']), '11'); 
+  });
+});
