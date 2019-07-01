e170160d95fa6a60240fbdeac529b87389a1ab37
Duncan Haran <duncan.haran@gmail.com>
2019-07-01 18:35:36 +0000

    seperating tests from functions and exporting modules for 'requires'

--- head.js
+++ head.js
@@ -1,18 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💯️💯️ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤬️🤬️🤬️ Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const assertEqual = require('./assertEqual')
 
 const head = function(array) {
   let item1 = array[0];
   return item1;
 };
 
-assertEqual(head([5]), 5);
-assertEqual(head([]), "Hello");
+module.exports = head;
--- tail.js
+++ tail.js
@@ -2,19 +2,8 @@ const assertEqual = require('./assertEqual')
 const assertArraysEqual = require('./assertArraysEqual')
 const equalArrays = require('./eqArrays')
 
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`💯️💯️ Assertion passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🤬️🤬️🤬️ Assertion failed: ${actual} !== ${expected}`);
-//   }
-// };
-
 const tail = function(array) {
   let result = array.slice(1);
   return result;
 };
 
-// console.log(tail([1,2,3,4]))
-
-console.log(assertArraysEqual(tail([1,2,3,4]), [2,3,4]));
